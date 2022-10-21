6886e13c2b5f175028c0ed92897fb9d120867d8e
deankp <dperez0689@gmail.com>
2022-10-20 20:28:41 -0500

    updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import axios from "axios";
 import Login from "./Components/Login";
 import SignUp from "./Components/SignUp";
+import Notes from "./Components/Notes";
 
 function App() {
   const [isLogin, setIsLogin] = useState(false);
+
   return (
     <div className="App">
-      <Login />
+      {isLogin ? <Notes /> : <Login />}
+      {/* <Login /> */}
       <SignUp />
     </div>
   );
