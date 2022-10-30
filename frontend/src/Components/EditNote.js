048363e082cc85d9fd759f4169c6b5cc7d30d7f7
John Weilert <mattweilert@gmail.com>
2022-10-30 17:38:14 -0500

    Done

--- frontend/src/Components/EditNote.js
+++ frontend/src/Components/EditNote.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
-import Delete from "../Images/delete.png";
 import "../CSS/CreateNote.css";
 import "../CSS/Animation.css";
 
