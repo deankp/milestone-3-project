273691a30f605ed64d8e2c515d6f32f918362507
deankp <dperez0689@gmail.com>
2022-11-01 10:49:25 -0500

    so you are saying there is a chance?

--- backend/server.js
+++ backend/server.js
@@ -37,9 +37,9 @@ mongoose.connect(process.env.MONGO_URL, () => {
 //Heroku attachment
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
   });
 }
 
