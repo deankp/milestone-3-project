49f8fd9887f7b4c314768f8c5fb3a499b144ae77
deankp <dperez0689@gmail.com>
2022-10-31 22:28:19 -0500

    so you are saying there is a chance?

--- backend/server.js
+++ backend/server.js
@@ -34,10 +34,12 @@ mongoose.connect(process.env.MONGO_URL, () => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
+  app.use(express.static(path.join(__dirname, "/../frontend/build")));
+  try {
+    res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+  } catch (e) {
+    res.send("Welcome to SoftNotes");
+  }
 }
 
 // Listen for Connections
